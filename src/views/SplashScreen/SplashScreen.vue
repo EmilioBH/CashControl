a2582c67626cb6ae94525082b3d1126cbbd45f0d
Emilio Jose Bonmati <emiliojosebonmati@MacBook-Pro-de-Emilio.local>
2023-12-09 16:25:40 +0100

    create splashscreen

---
+++ src/views/SplashScreen/SplashScreen.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="splashscreen">
+        <img src="../../assets/cashcontrol-icon.svg" data-testid="splash-icon" alt="cashcontrol-logo" height="250" width="250" />
+        <h1><span>Cash</span>Control</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.splashscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+h1 {
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
