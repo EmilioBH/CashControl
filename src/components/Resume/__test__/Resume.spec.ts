d35c4b7881ba4f15221d7a3d4142aae10e6588ee
Emilio Jose Bonmati <emiliojosebonmati@MacBook-Pro-de-Emilio.local>
2023-12-10 21:08:47 +0100

    resume init test

---
+++ src/components/Resume/__test__/Resume.spec.ts
@@ -0,0 +1,17 @@
+import { describe, test, expect } from 'vitest'
+import { screen, render } from '@testing-library/vue'
+import '@testing-library/jest-dom'
+import Resume from '..'
+
+describe('Resume Component', () => {
+    test('renders properly', () => {
+        render(Resume, {
+            props: {
+                amount: '1000'
+            }
+        })
+
+        expect(screen.getByText(/ahorro total/i)).toBeVisible()
+        expect(screen.getByText('1.000,00 €')).toBeVisible()
+    })
+})
