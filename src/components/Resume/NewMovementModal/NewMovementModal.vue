d9977ed7dc95c0a53c7b2ae7fe41c8fcd72cb9a7
Emilio Jose Bonmati <emiliojosebonmati@MacBook-Pro-de-Emilio.local>
2023-12-11 06:04:18 +0100

    add movement form

--- src/components/Resume/NewMovementModal/NewMovementModal.vue
+++ src/components/Resume/NewMovementModal/NewMovementModal.vue
@@ -5,15 +5,52 @@
             <i class="pi pi-times" style="font-size: 1.5rem; color: var(--brand-blue)" @click="closeModal" />
         </div>
         <div class="body">
-            Form will be here!
+            <form @submit.prevent="formSubmit">
+              <div class="field">
+                <label>Title</label>
+                <input type="text" v-model="title">
+              </div>
+              <div class="field">
+                <label>Amount</label>
+                <input type="number" v-model="amount">
+              </div>
+              <div class="field">
+                <label>Description</label>
+                <textarea rows="4" v-model="description"></textarea>
+              </div>
+              <div class="field">
+                <label class="radio-label">
+                  <input type="radio" v-model="movementType" value="Ingreso">
+                  <span>Ingreso</span>
+                </label>
+                <label class="radio-label">
+                  <input type="radio" v-model="movementType" value="Gasto">
+                  <span>Gasto</span>
+                </label>
+              </div>
+              <div class="action">
+                <button>Agregar</button>
+              </div>
+            </form>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 const emit = defineEmits(['closeModal'])
 
+const title = ref<string>('')
+const amount = ref<number>()
+const description = ref<string>('')
+const movementType = ref<'Ingreso'|'Gasto'>('Ingreso')
+
 const closeModal = () => emit('closeModal')
+
+const formSubmit = () => {
+  emit('closeModal')
+}
 </script>
 
 <style scoped>
@@ -40,5 +77,72 @@ p {
   margin: 0;
   font-size: 1.5rem;
   color: var(--brand-blue);
+}
+
+button {
+  color: white;
+  font-size: 1.25rem;
+  background-color: var(--brand-blue);
+  border: none;
+  width: 100%;
+  padding: 24px 60px;
+  border-radius: 60px;
+  box-sizing: border-box;
+}
+
+form {
+  font-size: 1.24rem;
+  width: 100%;
+}
+
+form .action {
+  padding: 0 24px;
+}
+
+.field {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  padding: 16px 24px;
+}
+
+label {
+  margin-bottom: 8px;
+}
+
+input,
+textarea {
+  font-size: 1.24rem;
+  border: 2px solid var(--brand-blue);
+  border-radius: 8px;
+  padding: 8px;
+}
+
+input[type="number"] {
+  text-align: right;
+}
+
+.radio-label {
+  display: flex;
+  align-items: center;
+  margin-top: 8px;
+}
+
+.radio-label span {
+  margin-top: 4px;
+  margin-left: 8px;
+}
+
+input[type="radio"] {
+  appearance: none;
+  width: 1.24rem;
+  height: 1.24rem;
+  color: var(--brand-blue);
+  border: 2px solid var(--brand-blue);
+  border-radius: 50%;
+}
+
+input[type="radio"]:checked {
+  background-color: var(--brand-blue);
 }
 </style>
